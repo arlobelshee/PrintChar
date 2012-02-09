5972365d2500e5f6261631244161e281fcae840f
Arlo Belshee <arlo.belshee@live.com>
2012-02-09 13:44:51 -0800

    Lifted one WPF-related helper class to the API assembly. This forced me to fix plugin reference and target framework problems.

--- PluginApi/Display/Helpers/VisibleIfValidConverter.cs
+++ PluginApi/Display/Helpers/VisibleIfValidConverter.cs
@@ -3,9 +3,9 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace PrintChar.Templates
+namespace PluginApi.Display.Helpers
 {
-	internal class VisibleIfValidConverter : IValueConverter
+	public class VisibleIfValidConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
