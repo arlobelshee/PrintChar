2e551b54483f55f74d0272dffe7347cb63795fbd
Arlo Belshee <arlo.belshee@live.com>
2012-02-18 22:52:08 -0800

    The game system can do some things related to finding character files. More to come.

--- PluginApi.Tests/DescribeAGameSystem.cs
+++ PluginApi.Tests/DescribeAGameSystem.cs
@@ -1,6 +1,7 @@
-﻿using NUnit.Framework;
-using PluginApi.Model;
+﻿using System.IO;
 using FluentAssertions;
+using NUnit.Framework;
+using PluginApi.Model;
 
 namespace PluginApi.Tests
 {
@@ -10,16 +11,55 @@ namespace PluginApi.Tests
 		[Test]
 		public void ShouldHaveAName()
 		{
-			var testSubject = new GameSystem("4th Edition D&D", "dnd4e");
-			testSubject.Name.Should().Be("4th Edition D&D");
+			_testSubject.Name.Should().Be("4th Edition D&D");
 		}
 
 		[Test]
 		public void ShouldDescribeFileFilters()
 		{
-			var testSubject = new GameSystem("4th Edition D&D", "dnd4e");
-			testSubject.FileExtension.Should().Be(".dnd4e");
-			testSubject.FilePattern.Should().Be("*.dnd4e");
+			_testSubject.FileExtension.Should().Be("dnd4e");
+			_testSubject.FilePattern.Should().Be("*.dnd4e");
+		}
+
+		[Test]
+		public void ShouldUseLabelAndExtensionCorrectlyToInitializeOpenDialog()
+		{
+			_testSubject.CreateOpenDialog().ShouldHave().SharedProperties().EqualTo(new
+			{
+				Filter = "4th Edition D&D file (*.dnd4e)|*.dnd4e",
+				DefaultExt = "dnd4e",
+				CheckFileExists = true,
+				Multiselect = false,
+				InitialDirectory = string.Empty
+			});
+		}
+
+		[Test]
+		public void IfHaveAlreadyOpenedCharacterShouldSetOpenDialogToStartInThatLocation()
+		{
+			_testSubject.CharacterFile = new CachedFile(new FileInfo(Path.GetTempPath() + @"ee.dnd4e"), false);
+			_testSubject.CreateOpenDialog().InitialDirectory.Should().Be(Path.GetTempPath().TrimEnd('\\'));
+		}
+
+		[Test]
+		public void ShouldAllowCreatingNewCharactersByDefault()
+		{
+			_testSubject.IsReadOnly.Should().BeFalse();
+		}
+
+		[Test]
+		public void ShouldAllowGameSystemsWhichCannotCreateCharacters()
+		{
+			_testSubject.IsReadOnly = true;
+			_testSubject.IsReadOnly.Should().BeTrue();
+		}
+
+		private GameSystem _testSubject;
+
+		[SetUp]
+		private void Setup()
+		{
+			_testSubject = new GameSystem("4th Edition D&D", "dnd4e");
 		}
 	}
 }
--- PluginApi/Model/GameSystem.cs
+++ PluginApi/Model/GameSystem.cs
@@ -1,16 +1,61 @@
+using System.Windows.Controls;
+using JetBrains.Annotations;
+using Microsoft.Win32;
+
 namespace PluginApi.Model
 {
 	public class GameSystem
 	{
+		[CanBeNull]
+		public CachedFile CharacterFile { get; set; }
+
 		public GameSystem(string systemLabel, string fileExtension)
 		{
 			Name = systemLabel;
-			FileExtension = string.Format(".{0}", fileExtension);
+			FileExtension = string.Format("{0}", fileExtension);
 			FilePattern = string.Format("*.{0}", fileExtension);
 		}
 
+		[NotNull]
 		public string Name { get; private set; }
+
+		[NotNull]
 		public string FileExtension { get; private set; }
+
+		[NotNull]
 		public string FilePattern { get; private set; }
+
+		public bool IsReadOnly { get; set; }
+
+		[NotNull]
+		public Control EditorDisplay { get; private set; }
+
+		public void SwitchCharacter()
+		{
+			_LoadCharacter(_Open(CreateOpenDialog()));
+		}
+
+		private void _LoadCharacter([CanBeNull] string fileName)
+		{
+		}
+
+		[CanBeNull]
+		private string _Open([NotNull] OpenFileDialog dialog)
+		{
+			return dialog.ShowDialog() == true ? dialog.FileName : null;
+		}
+
+		[NotNull]
+		public OpenFileDialog CreateOpenDialog()
+		{
+			return new OpenFileDialog
+			{
+				Filter = string.Format("{0} file ({1})|{1}", Name, FilePattern),
+				DefaultExt = FileExtension,
+				CheckFileExists = true,
+				Multiselect = false,
+				InitialDirectory = CharacterFile == null ? null : CharacterFile.Location.DirectoryName
+			};
+		}
 	}
 }
