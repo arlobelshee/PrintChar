0693672967863dbb1c3b6aca952315b8cc26ee88
Arlo Belshee <arlo.belshee@live.com>
2012-03-14 21:21:23 -0700

    Verifies that the plugin exports its game system.

---
+++ SenseOfWonder.Tests/PluginInterfaces.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+using FluentAssertions;
+using NUnit.Framework;
+using PluginApi.Model;
+
+namespace SenseOfWonder.Tests
+{
+	[TestFixture]
+	public class PluginInterfaces
+	{
+		[Test]
+		public void ShouldExportTheGameSystem()
+		{
+			var exportAsGameSystem = new ExportAttribute(typeof (GameSystem));
+			typeof (SenseOfWonderSystem).GetCustomAttributes(typeof (ExportAttribute), false)
+				.Should().Equal(exportAsGameSystem);
+		}
+	}
+}
--- SenseOfWonder/SenseOfWonderSystem.cs
+++ SenseOfWonder/SenseOfWonderSystem.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.ComponentModel.Composition;
+﻿using System.ComponentModel.Composition;
 using PluginApi.Model;
 
 namespace SenseOfWonder
 {
-	[Export(typeof(GameSystem))]
+	[Export(typeof (GameSystem))]
 	public class SenseOfWonderSystem : GameSystem
 	{
 		public SenseOfWonderSystem() : base("Sense of Wonder", "wonder") {}
