bf9096f004239349a455284bfe3af78aeb481422
Arlo Belshee <arlo.belshee@live.com>
2012-03-14 21:14:47 -0700

    Now successfully uses MEF to load a game system plugin. Everything is built to a destination that gives the right folder structure, and the plugin exports its game system.

--- SenseOfWonder/SenseOfWonderSystem.cs
+++ SenseOfWonder/SenseOfWonderSystem.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel.Composition;
 using PluginApi.Model;
 
 namespace SenseOfWonder
 {
+	[Export(typeof(GameSystem))]
 	public class SenseOfWonderSystem : GameSystem
 	{
 		public SenseOfWonderSystem() : base("Sense of Wonder", "wonder") {}
