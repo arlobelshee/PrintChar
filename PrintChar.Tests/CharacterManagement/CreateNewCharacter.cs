a77dbc345d165a08c82eade8db017ce515cb4934
Arlo Belshee <arlo.belshee@live.com>
2012-03-25 14:13:15 -0700

    Test cleanup.

--- PrintChar.Tests/CharacterManagement/CreateNewCharacter.cs
+++ PrintChar.Tests/CharacterManagement/CreateNewCharacter.cs
@@ -9,10 +9,29 @@ namespace PrintChar.Tests.CharacterManagement
 	[TestFixture]
 	public class CreateNewCharacter
 	{
-		private GameSystem _readOnlyGameSystem;
-		private GameSystem _anyGameSystem;
-		private GameSystem _writableGameSystem;
-		private AllGameSystemsViewModel testSubject;
+		[Test]
+		public void CreateShouldBeDisabledWithOnlyReadOnlySystemsPresent()
+		{
+			_With(_readOnlyGameSystem).CreateCharCommand.CanExecute(null).Should().BeFalse();
+		}
+
+		[Test]
+		public void CreateShouldBeEnabledWithAtLeastOneWritableSystemPresent()
+		{
+			_With(_readOnlyGameSystem, _writableGameSystem).CreateCharCommand.CanExecute(null).Should().BeTrue();
+		}
+
+		[Test]
+		public void CreateCharCommandShouldBeBoundToTheCorrectMethod()
+		{
+			var testSubject = _With(_anyGameSystem);
+			Assert.That(testSubject.CreateCharCommand, Command.DelegatesTo(() => testSubject.CreateNewCharacter()));
+		}
+
+		private static AllGameSystemsViewModel _With(params GameSystem[] gameSystems)
+		{
+			return new AllGameSystemsViewModel(gameSystems);
+		}
 
 		[SetUp]
 		public void Setup()
@@ -22,28 +41,8 @@ namespace PrintChar.Tests.CharacterManagement
 			_writableGameSystem = new _WritableGameSystem();
 		}
 
-		[Test]
-		public void WithOnlyReadOnlySystemsCreateShouldBeDisabled()
-		{
-			_With(_readOnlyGameSystem).CreateCharCommand.CanExecute(null).Should().BeFalse();
-		}
-
-		[Test]
-		public void WithSomeWritableSystemsCreateShouldBeEnabled()
-		{
-			_With(_readOnlyGameSystem, _writableGameSystem).CreateCharCommand.CanExecute(null).Should().BeTrue();
-		}
-
-		[Test]
-		public void CreateCharCommandShouldBeBoundToTheCorrectMethod()
-		{
-			testSubject = _With(_anyGameSystem);
-			Assert.That(testSubject.CreateCharCommand, Command.DelegatesTo(() => testSubject.CreateNewCharacter()));
-		}
-
-		private static AllGameSystemsViewModel _With(params GameSystem[] gameSystems)
-		{
-			return new AllGameSystemsViewModel(gameSystems);
-		}
+		private GameSystem _readOnlyGameSystem;
+		private GameSystem _anyGameSystem;
+		private GameSystem _writableGameSystem;
 	}
 }
